7bc3d9b11670d174b4999a8f85f97ff6c650b009
harshdev2909 <harsh.dev2909@gmail.com>
2024-08-23 20:48:53 +0530

    Frontend Done

--- ai/src/components/imageGenerator/ImageGenerator.jsx
+++ ai/src/components/imageGenerator/ImageGenerator.jsx
@@ -3,33 +3,6 @@ import './Image.css'
 import image from '../assets/default_image.svg'
 function ImageGenerator() {
 
-  const[image_url,setImage_url] = useState("/");
-  let inputRef = useRef(null);
-
-  const ImageGenerator = async ()=>{
-    if(inputRef.current.value===""){
-      return 0;
-    }
-    const response = await fetch(
-      "",
-      {
-        method : "POST",
-        headers:{
-          "Content-Type" : "application/json",
-          Authorization : " Bearer",
-          "User-Agent" : "Chrome",
-        },
-        body:JSON.stringify({
-          prompt : `${inputRef.current.value}`,
-          n:1,
-          size : "512x512"
-        }),
-      }
-      
-    );
-    let data = await response.json();
-    console.log(data)
-  }
   return (
     <div className='ai-image-generator'>
       <div className="header">Ai image <span>Generator</span></div>
